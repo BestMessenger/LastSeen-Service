7321ecc8bffb1acfb7354e200597b6baacb443e9
Denis Durbalov <den.dyrbalov25@gmail.com>
2023-08-11 10:31:18 +0200

    first commit

---
+++ src/main/java/com/messenger/lastseen_service/LastSeenServiceApplication.java
@@ -0,0 +1,13 @@
+package com.messenger.lastseen_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LastSeenServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LastSeenServiceApplication.class, args);
+    }
+
+}
