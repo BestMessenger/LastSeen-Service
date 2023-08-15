02b20f34c5db58b1b217d0e9aab1d4e470e7bce3
Denis Durbalov <den.dyrbalov25@gmail.com>
2023-08-15 14:16:48 +0200

    Dockerfile

--- src/main/java/com/messenger/lastseen_service/LastSeenServiceApplication.java
+++ src/main/java/com/messenger/lastseen_service/LastSeenServiceApplication.java
@@ -1,9 +1,11 @@
 package com.messenger.lastseen_service;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition
 public class LastSeenServiceApplication {
 
     public static void main(String[] args) {
