d14a513ab329f287c28274dc54fa3e6d5ee20a7d
Denis Durbalov <den.dyrbalov25@gmail.com>
2023-08-29 21:01:44 +0200

    update

--- src/main/java/com/messenger/lastseen_service/LastSeenServiceApplication.java
+++ src/main/java/com/messenger/lastseen_service/LastSeenServiceApplication.java
@@ -1,11 +1,14 @@
 package com.messenger.lastseen_service;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-@OpenAPIDefinition
+@OpenAPIDefinition(info = @Info(title = "LastSeen API",version = "1.0",description = "Documentation LastSeen API v1.0"))
+@EnableDiscoveryClient
 public class LastSeenServiceApplication {
 
     public static void main(String[] args) {
